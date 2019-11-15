060197ad1f2e7cd1185b5e5865cdfe7da27afcef
jingyuexing <1941755817@qq.com>
2019-11-16 07:35:02 +0800

    移除一些东西

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
 * @Author: Jingyuexing
 * @Date:   2018-12-31 21:46:27
-* @Last Modified by:   Jingyuexing
-* @Last Modified time: 2019-07-03 16:39:50
+* @Last Modified by:   jingyuexing
+* @Last Modified time: 2019-11-10 22:25:03
 */
 /**
 * router Module
@@ -19,18 +19,9 @@ main.config(['$routeProvider',function($routeProvider) {
     $routeProvider.when("/user",{
         templateUrl:"tpls/user.html",
         controller:"userCtrl",
-    }).when("/hot",{
-        templateUrl:"tpls/hot.html",
-        controller:"hotCtrl",
-    }).when("/edu",{
-        templateUrl:"tpls/edu.html",
-        controller:"eduCtrl",
     }).when("/kind",{
         templateUrl:"tpls/kind.html",
         controller:"kindCtrl",
-    }).when("/yul",{
-        templateUrl:"tpls/yul.html",
-        controller:"yulCtrl",
     }).when("/home",{
         templateUrl:"tpls/home.html",
         controller:"homeCtrl",
@@ -40,21 +31,6 @@ main.config(['$routeProvider',function($routeProvider) {
     }).when("/aboutme",{
         templateUrl:"tpls/aboutme.html",
         controller:"meCtrl"
-    }).when("/login",{
-        templateUrl:"tpls/login.html",
-        controller:"loginCtrl"
-    }).when("/test",{
-        templateUrl:"tpls/test.html",
-        controller:"testCtrl"
-    }).when("/soreline",{
-        templaterUrl:"tpls/scoreline.html",
-        controller:"scoreline"
-    }).when("/msg",{
-        templaterUrl:"tpls/msg.html",
-        controller:"msgCtrl"
-    }).when("/wether",{
-        templateUrl:"tpls/wether.html",
-        controller:"wetherCtrl"
     }).otherwise("/kind");
 }]);
 (function(){
--- src/yul.js
+++ src/yul.js
@@ -1,8 +1,8 @@
 /*
 * @Author: Jingyuexing
 * @Date:   2018-12-31 23:59:31
-* @Last Modified by:   Jingyuexing
-* @Last Modified time: 2019-06-28 17:14:40
+* @Last Modified by:   jingyuexing
+* @Last Modified time: 2019-10-11 17:24:09
 */
 main.controller('yulCtrl', ['$scope', function($scope){
 }]);
