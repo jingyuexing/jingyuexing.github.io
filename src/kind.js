cb83f57e682c280c8a38d01aa1fb078e8bcbbd11
jingyuexing <1941755817@qq.com>
2019-11-30 23:37:18 +0800

    normal:增加调试代码

--- src/kind.js
+++ src/kind.js
@@ -2,22 +2,23 @@
 * @Author: Jingyuexing
 * @Date:   2018-12-31 23:58:56
 * @Last Modified by:   Admin
-* @Last Modified time: 2019-11-30 23:22:40
+* @Last Modified time: 2019-11-30 23:32:35
 */
 main.controller('kindCtrl', ['$scope',"$http", function($scope,$http){
-	var times = new Date();
-	times = times.getTime();
-	$scope.enbale=function(){
-		console.log(this);
-	}
-	$http({
-		url:"./DB/kind.list?t="+times,
-		method:"GET",
-		type:"json"
-	}).then(function(res){
-		$scope.link = angular.fromJson(res.data);
-		console.log($scope.link);
-	}, function(res){
-		console.log("失败!");
-	});
+    var times = new Date();
+    times = times.getTime();
+    $http({
+        url:"./DB/kind.list?t="+times,
+        method:"GET",
+        type:"json"
+    }).then(function(res){
+        $scope.link = angular.fromJson(res.data);
+        console.log($scope.link);
+    }, function(res){
+        console.log("失败!");
+    });
+    $scope.enbale=function(){
+		let tag = this.tag;
+		console.log(tag);
+    }
 }]);
