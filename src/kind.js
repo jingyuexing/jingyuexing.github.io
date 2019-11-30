4f547539419c56bd61b0226040d85c860c8f2494
jingyuexing <1941755817@qq.com>
2019-11-30 23:24:12 +0800

    Features:增加按标签显示功能

--- src/kind.js
+++ src/kind.js
@@ -1,12 +1,15 @@
 /*
 * @Author: Jingyuexing
 * @Date:   2018-12-31 23:58:56
-* @Last Modified by:   jingyuexing
-* @Last Modified time: 2019-10-01 12:29:50
+* @Last Modified by:   Admin
+* @Last Modified time: 2019-11-30 23:22:40
 */
 main.controller('kindCtrl', ['$scope',"$http", function($scope,$http){
 	var times = new Date();
 	times = times.getTime();
+	$scope.enbale=function(){
+		console.log(this);
+	}
 	$http({
 		url:"./DB/kind.list?t="+times,
 		method:"GET",
