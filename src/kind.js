89092c4dc8fdb6648d40d7488b2ad0735b8b91fe
jingyuexing <1941755817@qq.com>
2019-12-01 19:15:09 +0800

    features:增加功能

--- src/kind.js
+++ src/kind.js
@@ -1,37 +1,36 @@
 /*
-* @Author: Jingyuexing
-* @Date:   2018-12-31 23:58:56
-* @Last Modified by:   Admin
-* @Last Modified time: 2019-12-01 01:08:22
-*/
-main.controller('kindCtrl', ['$scope',"$http", function($scope,$http){
+ * @Author: Jingyuexing
+ * @Date:   2018-12-31 23:58:56
+ * @Last Modified by:   Admin
+ * @Last Modified time: 2019-12-01 19:07:04
+ */
+main.controller('kindCtrl', ['$scope', "$http", function($scope, $http) {
     var times = new Date();
     times = times.getTime();
     $http({
-        url:"./DB/kind.list?t="+times,
-        method:"GET",
-        type:"json"
-    }).then(function(res){
+        url: "./DB/kind.list?t=" + times,
+        method: "GET",
+        type: "json"
+    }).then(function(res) {
         $scope.link = angular.fromJson(res.data);
-        console.log($scope.link);
-    }, function(res){
+        $scope.backup = $scope.links;
+    }, function(res) {
         console.log("失败!");
     });
-    $scope.enbale=function(){
+    $scope.enbale = function() {
         $scope.cacheTag = this.tag;
-        let temp = $scope.link;
+        let temp = $scope.backup;
         let cache = [];
-        for(let i=0;i<temp.length;i++){
-            if(temp[i].tag.includes($scope.cacheTag)){
+        for (let i = 0; i < temp.length; i++) {
+            if (temp[i].tag.includes($scope.cacheTag)) {
                 cache.push(temp[i]);
             }
         }
         $scope.link = cache;
-        $scope.$watch("cacheTag", function(newVal, oldVal) {
-            if(newVal!=oldVal){
-                $scope.link = cache;
-                $scope.status ="changed";
-            }
-        });
     }
+    $scope.$watch("cacheTag", function(newVal, oldVal) {
+        if (newVal != oldVal) {
+            $scope.status = "changed";
+        }
+    });
 }]);
