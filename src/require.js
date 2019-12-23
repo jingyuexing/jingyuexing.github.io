966509d3e5104532cf6a0f5f8332d5877d828836
jingyuexing <1941755817@qq.com>
2019-12-23 23:25:42 +0800

    fix:reuire.js can not import css files

--- src/require.js
+++ src/require.js
@@ -26,36 +26,53 @@ function fileInfo(fileName=''){
     return info;
 }
 /**
- * 引入JS文件
+ * 在网页中引入JS文件
+ *```js
+ * //你可以引入单一的文件
+ * require("https://www.example.com/test.js")
+ * //你同样可以添加多个文件
+ * require(["https://www.example.com/test_v1.js","https://www.example.com/test_V2.js"])
+ * //你还可以添加CSS文件
+ * require("https://www.example.com/test.css")
+ * //仅仅支持这两种文件类型
+ *```
+ * @param   {string|Array<string>}  url  你所要添加的网页链接
  *
- * @param   {String}  url  文件链接
- *
- * @return  {Boolean}       如果有重复则不引入并返回`false`否则返回`true`
+ * @return  {boolean}      成功返回`true`失败返回`false` 如果引入重复会直接返回`false`
  */
-function require(url=''){
+function require(url = '') {
     function isArray(val) {
         return toString.call(val) === '[object Array]';
     }
+    var AddTag,headUrlList,cacheList=[],b;
+    var head = document.head
     if(url!=''&&fileInfo(url)["fileType"]=="js"){
-        var head = document.head;
-        var scriptTag = document.createElement("script");
-        scriptTag.async=true;
-        scriptTag.src=url;
-        var scriptList = document.querySelectorAll("head>script");
-        var srcList=[];
-        for(let m =0;m<scriptList.length;m++){
-            srcList.push(scriptList[m].src);
-        }
-        if(srcList.includes(scriptTag.src)){
-            return false;
-        }else{
-            head.appendChild(scriptTag);
-            return true;
-        }
+        AddTag = document.createElement("script");
+        AddTag.src=url;
+        AddTag.async=true;
+        headUrlList = document.querySelectorAll("head>script");
+        headUrlList.forEach((ele)=>{
+            cacheList.push(ele.src);
+        });
+    }else if(url!=''&&fileInfo(url)['fileType']=="css"){
+        AddTag = document.createElement("link");
+        AddTag.rel="stylesheet";
+        AddTag.href=url
+        headUrlList = document.querySelectorAll("head>link");
+        headUrlList.forEach((ele)=>{
+            cacheList.push(ele.href);
+        });
     }else if(isArray(url)){
-        for(var x=0;x<url.length;x++){
-            require(url[x]);
-        }
+        url.forEach((ele)=>{
+            require(ele);
+        })
+    }else{
+        return false;
     }
-    return false;
+    if (cacheList.includes(url)) {
+            return false;
+    } else {
+            head.appendChild(AddTag);
+            return true;
+    }
 }
