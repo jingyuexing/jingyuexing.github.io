24a21cde7bf7f029d2ef12d8731cda074809fd06
jingyuexing <1941755817@qq.com>
2020-08-25 09:45:00 +0800

    删除不必要内容

--- src/user.js
+++ src/user.js
@@ -2,17 +2,8 @@
 * @Author: Jingyuexing
 * @Date:   2019-01-01 10:01:46
 * @Last Modified by:   Jingyuexing
-* @Last Modified time: 2020-08-20 21:41:57
+* @Last Modified time: 2020-08-25 09:44:22
 */
 
 main.controller('userCtrl', ['$scope', function($scope){
 }]);
-var data = {};var ary = []
-document.querySelectorAll(".palette-card").forEach(ele=>{
-     data[ele.getAttribute("data-clip")]=[];
-     ele.childNodes
-     document.querySelectorAll(".palette-card-body div").forEach(item=>{
-        var color = /#[a-f0-9]{6}/ig;
-        ary.push(color.exec(item.getAttribute("style"))[0])
-     })
-})
