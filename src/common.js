e53bc894d9c3ea3fa0b37c44ca3b76af08806242
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2022-11-12 04:11:59 +0000

    feat: as  static function

--- src/common.js
+++ src/common.js
@@ -2,18 +2,19 @@ class Timer{
     constructor(){
         this.date = new Date();
     }
+
+}
     /**
      * 
      * @param {number} age 
      */
-    randomDate(age){
-        let currentYear = this.date.getFullYear();
+Timer.randomDate(age){
+        let currentYear = Timer.date.getFullYear();
         let randomSecond = 0; 
         var begin,end;
         if(age){
             begin = new Date(`${currentYear - age}-1-1`);
             end = new Date(`${currentYear - age}-12-31`); 
-            
         }else{
             begin = new Date(`${currentYear}-1-1`);
             end = new Date(`${currentYear}-12-31`); 
@@ -21,5 +22,5 @@ class Timer{
         randomSecond = Math.random() * (end.getTime() - begin.getTime()) + begin.getTime();
         return new Date(randomSecond);
     }
-}
-console.log(new Timer().randomDate())
+
+console.log(Timer.randomDate())
