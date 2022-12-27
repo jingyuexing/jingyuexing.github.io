80b5f02ad27716dc49ea39a32f5c03bb626ed0f9
jingyuexing <MayorTalor@163.com>
2022-12-28 01:02:46 +0800

    update feature

--- src/main.js
+++ src/main.js
@@ -1,125 +1,149 @@
 /*
 * @Author: Jingyuexing
 * @Date:   2018-12-31 21:46:27
-* @Last Modified by:   Jingyuexing
-* @Last Modified time: 2022-09-15 09:42:26
+* @Last Modified by:   admin
+* @Last Modified time: 2021-10-26 04:19:17
 */
 /**
 * router Module
 *
 * Description
 */
-function randomInt(scopeNum){
-    function IntParser(floatNumber=0){
+function randomInt(scopeNum) {
+    function IntParser(floatNumber = 0) {
         return ~~floatNumber;
     }
-    return IntParser(Math.random()*scopeNum);
+    return IntParser(Math.random() * scopeNum);
 }
-var main=angular.module('main', ['ngRoute']);
-main.service('Comm', function(){
+function $ready(func) {
+    document.onreadystatechange = function () {
+        if (document.readyState === "complete") {
+            func()
+        }
+    }
+}
+
+$ready(function () {
+    var themes = document.getElementById("themes");
+    var date = new Date();
+    if (date.getMonth() <= 1 && date.getDate() <= 26) {
+        themes.setAttribute("href", "asset/css/happynewyear.css")
+    } else {
+        themes.setAttribute("href", "asset/css/default.css")
+    }
+})
+
+var main = angular.module('main', ['ngRoute']);
+main.service('Comm', function () {
     var str = /(kind|test|login|msg|home|yul|sport|hot|edu)/;
-    var name = {"edu":"教育","hot":"热点","home":"主页","yul":"娱乐","msg":"消息","login":"注册","kind":"分类","user":"用户"};
-    this.TitleName=name[location.hash.match(str)[0]];
+    var name = { "edu": "教育", "hot": "热点", "home": "主页", "yul": "娱乐", "msg": "消息", "login": "注册", "kind": "分类", "user": "用户" };
+    this.TitleName = name[location.hash.match(str)[0]];
 });
-main.config(['$routeProvider',function($routeProvider) {
-    $routeProvider.when("/user",{
-        templateUrl:"tpls/user.html",
-        controller:"userCtrl",
-    }).when("/kind",{
-        templateUrl:"tpls/kind.html",
-        controller:"kindCtrl",
-    }).when("/sport",{
-        templateUrl:"tpls/sport.html",
-        controller:"sportCtrl"
-    }).when("/aboutme",{
-        templateUrl:"tpls/aboutme.html",
-        controller:"meCtrl"
+main.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when("/user", {
+        templateUrl: "tpls/user.html",
+        controller: "userCtrl",
+    }).when("/kind", {
+        templateUrl: "tpls/kind.html",
+        controller: "kindCtrl",
+    }).when("/home", {
+        templateUrl: "tpls/home.html",
+        controller: "homeCtrl",
+    }).when("/sport", {
+        templateUrl: "tpls/sport.html",
+        controller: "sportCtrl"
+    }).when("/aboutme", {
+        templateUrl: "tpls/aboutme.html",
+        controller: "meCtrl"
     }).when('/source', {
         templateUrl: 'source.html',
         controller: 'SourceCtrl'
     }).when('/data', {
         templateUrl: 'tpls/data.html',
         controller: 'DataCtrl'
+    }).when('/download', {
+        templateUrl: 'tpls/download.html',
+        controller: 'DownloadCtrl'
     }).when('/course', {
         templateUrl: 'tpls/course.html',
         controller: 'CourseCtrl'
-    }).when("/apidocs",{
-        templateUrl:"tpls/apidocs.html",
-        controller:"apidocsCtrl"
+    }).when('/resource', {
+        templateUrl: "tpls/resource.html",
+        controller: "resCtrl"
     })
-    .otherwise("/kind");
+        .otherwise("/kind");
 }]);
-(function(){
+(function () {
     var str = /(kind|test|login|msg|home|yul|sport|hot|edu|user|wether|aboutme)/;
-switch (location.hash.match(str)[0]) {
-    case "kind":
-        document.title="分类";
-        // statements_1
-        break;
-    case "login":
-        document.title="登陆";
-        break;
-    case "hot":
-        document.title="热点";
-        break;
-    case "yul":
-        document.title="娱乐";
-        break;
-    case "test":
-        document.title="测试";
-        break;
-    case "msg":
-        document.title="消息";
-        break;
-    case "home":
-        document.title="主页";
-        break;
-    case "sport":
-        document.title="运动";
-        break;
-    case "edu":
-        document.title="教育";
-        break;
-    case "user":
-        document.title="用户";
-        break;
-    case "wether":
-        document.title="天气预报";
-        break;
-    case "aboutme":
-        document.title="关于我";
-        break;
-}
+    switch (location.hash.match(str)[0]) {
+        case "kind":
+            document.title = "分类";
+            // statements_1
+            break;
+        case "login":
+            document.title = "登陆";
+            break;
+        case "hot":
+            document.title = "热点";
+            break;
+        case "yul":
+            document.title = "娱乐";
+            break;
+        case "test":
+            document.title = "测试";
+            break;
+        case "msg":
+            document.title = "消息";
+            break;
+        case "home":
+            document.title = "主页";
+            break;
+        case "sport":
+            document.title = "运动";
+            break;
+        case "edu":
+            document.title = "教育";
+            break;
+        case "user":
+            document.title = "用户";
+            break;
+        case "wether":
+            document.title = "天气预报";
+            break;
+        case "aboutme":
+            document.title = "关于我";
+            break;
+    }
 })();
 
-var clock = function(){
+var clock = function () {
     var str = ""
     var h = new Date().getHours();
-    if(0<=h&&h<5){
+    if (0 < h && h < 5) {
         //凌晨
         str = "凌晨";
-    }else if(5<=h&&h<9){
+    } else if (5 <= h && h < 9) {
         //早上
         str = "早上";
-    }else if(9<=h&&h<12){
+    } else if (9 <= h && h < 12) {
         //上午
         str = "上午";
-    }else if(12<=h&&h<13){
+    } else if (12 <= h && h < 13) {
         //中午
         str = "中午";
-    }else if(13<=h&&h<15){
+    } else if (13 <= h && h < 15) {
         //下午
         str = "下午";
-    }else if(15<=h&&h<20){
+    } else if (15 <= h && h < 20) {
         //旁晚
         str = "旁晚";
-    }else if(20<=h&&h<22){
+    } else if (20 <= h && h < 22) {
         //晚上
         str = "晚上";
-    }else if(22<=h&&h<=0){
+    } else if (22 <= h && h <= 0) {
         //午夜
         str = "午夜";
     }
     return str;
 }
-console.log("%c特么又来看源码!","background-color:#2eb7ed;padding:4px 6px;color:#fff;border-radius:4px 4px 4px 4px;font-size:2em;")
+console.log("%c特么又来看源码!", "background-color:#2eb7ed;padding:4px 6px;color:#fff;border-radius:4px 4px 4px 4px;font-size:2em;")
